f11896caaf99b4590fc576bf8f59d6514cd513c5
Alex linux <xnet141@gmail.com>
2025-01-25 00:50:02 +0300

    lesson 3 linux

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,49 @@
 class QuestionsController < ApplicationController
+  def show
+    @question = Question.find params[:id]
+  end
+  
+  def destroy
+    @question = Question.find params[:id]
+    @question.destroy
+    redirect_to questions_path
+  end
+
+  def edit
+    @question = Question.find params[:id]
+  end
+
+  def update
+    @question = Question.find params[:id]
+    if @question.update question_params
+      redirect_to questions_path
+    else
+      render :edit
+    end
+  end
+  
   def index
     @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+    # render plain: params
+  end
+
+  def create
+    @question = Question.new question_params
+    if @question.save
+      redirect_to questions_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get '/questions', to: 'questions#index'
-
+  resources :questions, only: %i[index new edit create show update destroy]
+  
+  # get '/questions', to: 'questions#index'
+  # get '/questions/new', to: 'questions#new'
+  # get '/questions/:id', to: 'questions#show'
+  # get '/questions/:id/edit', to: 'questions#edit'
+  # post '/questions', to: 'questions#create'
+  
   root "pages#index"
 end
