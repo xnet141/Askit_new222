907d25a7c19957837ea7bedeece9d96c98aea311
Alex Erofeev <xnet141@gmail.com>
2025-01-30 19:25:38 +0300

    lesson 8 ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,16 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
   include Pagy::Backend
   include ErrorHandling
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id].present?
+  end
+
+  def user_signed_in?
+    current_user.present?
+  end
+
+  helper_method :current_user, :user_signed_in?
 end
